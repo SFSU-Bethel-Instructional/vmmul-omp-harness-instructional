26a5581a3d6d2b2382290ac4a729d0aa2e0b2a3b
Omar Dajani <omardajani079@gmail.com>
2023-11-13 11:01:28 -0800

    Some basic code

--- benchmark.cpp
+++ benchmark.cpp
@@ -5,111 +5,113 @@
 //
 // usage: no command line arguments
 // set problem sizes in the code below
-
 #include <algorithm>
 #include <chrono>
 #include <iomanip>
 #include <iostream>
 #include <random>
 #include <vector>
-
 #include <cmath> // For: fabs
-
 #include <cblas.h>
 #include <string.h>
 
 // external definitions for mmul's
-extern void my_dgemv(int, double*, double*, double *);
-extern const char* dgemv_desc;
+extern void my_dgemv(int, double *, double *, double *);
+extern const char *dgemv_desc;
 
-void reference_dgemv(int n, double* A, double* x, double *y) {
-   double alpha=1.0, beta=1.0;
-   int lda=n, incx=1, incy=1;
-    // cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, n, n, alpha, A, n, B, n, 1., C, n);
-    cblas_dgemv(CblasRowMajor, CblasNoTrans, n, n, alpha, A, lda, x, incx, beta, y, incy);
+void reference_dgemv(int n, double *A, double *x, double *y)
+{
+  double alpha = 1.0, beta = 1.0;
+  int lda = n, incx = 1, incy = 1;
+  // cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, n, n, alpha, A, n, B, n, 1., C, n);
+  cblas_dgemv(CblasRowMajor, CblasNoTrans, n, n, alpha, A, lda, x, incx, beta, y, incy);
 }
 
-void fill(double* p, int n) {
-    static std::random_device rd;
-    static std::default_random_engine gen(rd());
-    static std::uniform_real_distribution<> dis(-1.0, 1.0);
-    for (int i = 0; i < n; ++i)
-        p[i] = 2 * dis(gen) - 1;
+void fill(double *p, int n)
+{
+  static std::random_device rd;
+  static std::default_random_engine gen(rd());
+  static std::uniform_real_distribution<> dis(-1.0, 1.0);
+  for (int i = 0; i < n; ++i)
+    p[i] = 2 * dis(gen) - 1;
 }
 
 bool check_accuracy(double *A, double *Anot, int nvalues)
 {
   double eps = 1e-5;
-  for (size_t i = 0; i < nvalues; i++) 
+  for (size_t i = 0; i < nvalues; i++)
   {
-    if (fabsf(A[i] - Anot[i]) > eps) {
-       return false;
+    if (fabsf(A[i] - Anot[i]) > eps)
+    {
+      return false;
     }
   }
   return true;
 }
 
-
 /* The benchmarking program */
-int main(int argc, char** argv) 
+int main(int argc, char **argv)
 {
-    std::cout << "Description:\t" << dgemv_desc << std::endl << std::endl;
+  std::cout << "Description:\t" << dgemv_desc << std::endl
+            << std::endl;
 
-    std::cout << std::fixed << std::setprecision(5);
+  std::cout << std::fixed << std::setprecision(5);
 
-    // we purposefully run the smallest problem twice so as to "condition"
-    // BLAS. For timing purposes, ignore the timing of the first problem size
-    std::vector<int> test_sizes{1024, 1024, 2048, 4096, 8192, 16384};
+  // we purposefully run the smallest problem twice so as to "condition"
+  // BLAS. For timing purposes, ignore the timing of the first problem size
+  std::vector<int> test_sizes{1024, 1024, 2048, 4096, 8192, 16384};
 
-    int n_problems = test_sizes.size();
+  int n_problems = test_sizes.size();
 
-    // preallocate memory buffers for all problems: assume the last number in test_sizes is the largest
+  // preallocate memory buffers for all problems: assume the last number in test_sizes is the largest
 
-    // allocate memory for 2 NxN matrices and 4 Nx1 vectors
+  // allocate memory for 2 NxN matrices and 4 Nx1 vectors
 
-    int max_size = test_sizes[n_problems-1];
+  int max_size = test_sizes[n_problems - 1];
 
-    std::vector<double> buf(2 * max_size * max_size + 4 * max_size);
-    double* A = buf.data() + 0;
-    double* Acopy = A + max_size * max_size;
-    double* X = Acopy + max_size * max_size;
-    double* Xcopy = X + max_size;
-    double* Y = Xcopy + max_size;
-    double* Ycopy = Y + max_size;
+  std::vector<double> buf(2 * max_size * max_size + 4 * max_size);
+  double *A = buf.data() + 0;
+  double *Acopy = A + max_size * max_size;
+  double *X = Acopy + max_size * max_size;
+  double *Xcopy = X + max_size;
+  double *Y = Xcopy + max_size;
+  double *Ycopy = Y + max_size;
 
-           // load up matrics with some random numbers
-    /* For each test size */
-    for (int n : test_sizes) 
-    {
-        printf("Working on problem size N=%d \n", n);
+  // load up matrics with some random numbers
+  /* For each test size */
+  for (int n : test_sizes)
+  {
+    printf("Working on problem size N=%d \n", n);
 
-        fill(A, n * n);
-        fill(X, n );
-        fill(Y, n );
+    fill(A, n * n);
+    fill(X, n);
+    fill(Y, n);
 
-        // make copies of A, B, C for use in verification of results
-        memcpy((void *)Acopy, (const void *)A, sizeof(double)*n*n);
-        memcpy((void *)Xcopy, (const void *)X, sizeof(double)*n);
-        memcpy((void *)Ycopy, (const void *)Y, sizeof(double)*n);
+    // make copies of A, B, C for use in verification of results
+    memcpy((void *)Acopy, (const void *)A, sizeof(double) * n * n);
+    memcpy((void *)Xcopy, (const void *)X, sizeof(double) * n);
+    memcpy((void *)Ycopy, (const void *)Y, sizeof(double) * n);
 
-        // insert start timer code here
+    // insert start timer code here
+    auto start = std::chrono::high_resolution_clock::now();
+    // call the method to do the work
+    my_dgemv(n, A, X, Y);
 
-        // call the method to do the work
-        my_dgemv(n, A, X, Y); 
+    // insert end timer code here, and print out the elapsed time for this problem size
+    auto end = std::chrono::high_resolution_clock::now();
 
-        // insert end timer code here, and print out the elapsed time for this problem size
+    double duration = std::chrono::duration<double>(end - start).count();
 
+    // now invoke the cblas method to compute the matrix-vector multiplye
+    reference_dgemv(n, Acopy, Xcopy, Ycopy);
 
-        // now invoke the cblas method to compute the matrix-vector multiplye
-        reference_dgemv(n, Acopy, Xcopy, Ycopy);
+    // compare your result with that computed by BLAS
+    if (check_accuracy(Ycopy, Y, n) == false)
+      printf(" Error: your answer is not the same as that computed by BLAS. \n");
 
-        // compare your result with that computed by BLAS
-        if (check_accuracy(Ycopy, Y, n) == false)
-           printf(" Error: your answer is not the same as that computed by BLAS. \n");
-    
-    } // end loop over problem sizes
+  } // end loop over problem sizes
 
-    return 0;
+  return 0;
 }
 
 // EOF
--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -1,4 +1,4 @@
-const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
+const char *dgemv_desc = "Basic implementation of matrix-vector multiply.";
 
 /*
  * This routine performs a dgemv operation
@@ -6,6 +6,15 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
-void my_dgemv(int n, double* A, double* x, double* y) {
+void my_dgemv(int n, double *A, double *x, double *y)
+{
    // insert your code here: implementation of basic matrix multiply
+   for (int row = 0; row < n; row++)
+   {
+      int offset = row * n;
+      for (int column = 0; column < n; column++)
+      {
+         y[offset + column] += A[row * n + column] * x[column];
+      }
+   }
 }
